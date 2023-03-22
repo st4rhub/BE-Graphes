55badbc89202b009d5eb7560dc12f0fed86b4605
st4rhub <oceanepioche@gmail.com>
2023-03-22 10:19:51 +0100

    getTravelTime() was implemented

--- be-graphes-model/src/main/java/org/insa/graphs/model/Path.java
+++ be-graphes-model/src/main/java/org/insa/graphs/model/Path.java
@@ -225,11 +225,9 @@ public class Path {
      * @return Time (in seconds) required to travel this path at the given speed (in
      *         kilometers-per-hour).
      * 
-     * @deprecated Need to be implemented.
      */
     public double getTravelTime(double speed) {
-        // TODO:
-        return 0;
+        return getLength() * 3600.0 / (speed * 1000.0);
     }
 
     /**
